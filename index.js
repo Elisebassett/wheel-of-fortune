78e7509a3fad3479632480e4be7b9565e762ec72
Elisebassett <elise@iileaders.com>
2017-10-23 15:58:39 -0600

    fixes #5

--- index.js
+++ index.js
@@ -4,73 +4,77 @@ $(function () {
 	}//if
 	class Game {
 		constructor() {
-			this.wordArray = [['a','p','p','l','e'], ['n','a','g','g','e','r']];
+			this.wordArray = [['A','P','P','L','E'], ['N','A','G','G','E','R']];
 			this.word = null;
 			this.game_count = parseInt(Cookies.get('gameCount'));
+			this.letterArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+			this.guessArray = [];
 		}//constructor
 
 		letsPlay() {
+			let col = `<div class="col-1 grid"><img src="imgs/wheel_logo.png"></div>`;
+			let end_row = `<div class="row">
+				          <div class="col-1 grid end"></div>
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          <div class="col-1 grid end"></div>
+				        </div> <!-- row -->`;
+			let middle_row = `<div class="row">
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				          ${col}
+				        </div> <!-- row -->`;
+
 			if (this.game_count === 0) {
-				$('#display').html(`
+				$('#display').append(`
 					<div class="container">
+				        ${end_row}
 				        <div class="row">
-				          <div class="col-1 end"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 end"></div>
+				        	${col}
+				        	${col}
+				          <div class="col-1 grid" data-letter="a"><img src="imgs/wheel_logo.png"></div>
+				          <div class="col-1 grid" data-letter="p"><img src="imgs/wheel_logo.png"></div>
+				          <div class="col-1 grid" data-letter="p"><img src="imgs/wheel_logo.png"></div>
+				          <div class="col-1 grid" data-letter="l"><img src="imgs/wheel_logo.png"></div>
+				          <div class="col-1 grid" data-letter="e"><img src="imgs/wheel_logo.png"></div>
+				        	${col}
+				        	${col}
+				        	${col}
+				        	${col}
+				        	${col}
 				        </div> <!-- row -->
-				        <div class="row">
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 a" data-letter="a"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 p" data-letter="p"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 p" data-letter="p"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 l" data-letter="l"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 e" data-letter="e"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				        </div> <!-- row -->
-				        <div class="row">
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				        </div> <!-- row -->
-				        <div class="row">
-				          <div class="col-1 end"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1"><img src="imgs/wheel_logo.png"></div>
-				          <div class="col-1 end"></div>
-				        </div> <!-- row -->
+				        ${middle_row}
+				      	${end_row}
 				      </div> <!-- container --> 
 				`);//end game 0 append
+				for (var i = 0; i < this.letterArray.length; i++) {
+					$('#keyboard').append(`
+						<div class="col">
+							<button type="button" class="btn btn-outline-info key" id="${this.letterArray[i]}" data-letter="${this.letterArray[i]}">${this.letterArray[i]}</button>
+						</div>
+					`);//keyboard append
+				} //for loop
+				$('#guessInput').append(`
+					<input type="text" id="guess" placeholder="Think you know what it is?">
+					<button id="submit_guess">Guess</button>
+				`);//guessInput
 			}//if game 0
 
 		}//letsPlay
@@ -80,7 +84,7 @@ $(function () {
 		}//guessLetter
 
 		checkGuess() {
-			let guess = $('#guess').val();
+			let guess = $('#guess').val().toUpperCase();
 			this.word = this.wordArray[this.game_count].join('');
 			console.log(this.word);
 			// console.log(second_word);
@@ -107,8 +111,9 @@ $(function () {
 		wheel.letsPlay();
 		// Cookies.set('gameCount', this.game_count + 1);
 	});
+	$(document).on('click', '.key', function(){
 
-
+	});
 
 
 
