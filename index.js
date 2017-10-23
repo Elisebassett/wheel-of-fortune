7758709b22eeed8abc16c5309a88956ba9e692dc
Elisebassett <elise@iileaders.com>
2017-10-23 11:13:29 -0600

    check guess

--- index.js
+++ index.js
@@ -2,31 +2,42 @@ $(function () {
 
 	class Game {
 		constructor() {
-			this.wordArray = ['a','p','p','l','e'];
+			this.wordArray = [['a','p','p','l','e'], ['n','a','g','g','e','r']];
 
 			
 		}//constructor
 
+		letsPlay() {
+
+		}//letsPlay
+
 		guessLetter() {
 
-		}
+		}//guessLetter
 
-		checkGuess() {
+		checkGuessOne() {
 			let guess = $('#guess').val();
-			var word = this.wordArray.join('');
-			console.log(word);
-			if (word === guess) {
+			// var word = [this.wordArray.shift()].join('');
+			var first_word = this.wordArray[0].join('');
+			// var second_word = this.wordArray[1].join('');
+			// console.log(word);
+			console.log(first_word);
+			// console.log(second_word);
+			if (first_word === guess) {
 				alert('You did the thing!');
 
 			} else {
+				alert('nope')
 
 			}//if else
-		}//checkGuess
+		}//checkGuessOne
 
 	}//Game
 
 	var wheel = new Game();
-	$('#submit_guess').click(wheel.checkGuess());
+	$(document).on('click', '#submit_guess', function(){
+		wheel.checkGuessOne();
+	});
 
 
 
